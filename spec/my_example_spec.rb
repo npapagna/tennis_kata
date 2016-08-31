b0160ccf6eaee3248936180da3569a670d0fb280
Nicolas Papagna Maldonado <nicolas.papagna@8thlight.com>
2016-08-31 15:39:40 -0300

    Test 008
    Opponent wins a point after a player loses a point

--- spec/my_example_spec.rb
+++ spec/my_example_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 class TennisGame
 
   def initialize
+    @player_1_serving = true
     @player_1_score = 0
+    @player_2_score = 0
   end
 
   def player_1_score
@@ -11,35 +13,39 @@ class TennisGame
   end
 
   def player_2_score
-    0
+    @player_2_score
   end
 
   def win_point
-    if @player_1_score == 0
-      @player_1_score = 15
-      return
-    end
+    if @player_1_serving
+      if @player_1_score == 0
+        @player_1_score = 15
+        return
+      end
 
-    if @player_1_score == 15
-      @player_1_score = 30
-      return
-    end
+      if @player_1_score == 15
+        @player_1_score = 30
+        return
+      end
 
-    if @player_1_score == 30
-      @player_1_score = 40
-      return
-    end
+      if @player_1_score == 30
+        @player_1_score = 40
+        return
+      end
 
-    if @player_1_score == 40
-      @player_1_score = :game
-      return
-    end
+      if @player_1_score == 40
+        @player_1_score = :game
+        return
+      end
 
-    fail 'Game over'
+      fail 'Game over'
+    else
+      @player_2_score = 15
+    end
   end
 
   def lose_point
-
+    @player_1_serving = false
   end
 
 end
@@ -104,9 +110,16 @@ describe 'My behaviour' do
     assert_game_score_is love, love
   end
 
+  it '008' do
+    tennis_game.lose_point
+    tennis_game.win_point
+
+    assert_game_score_is love, fifteen
+  end
+
   def assert_game_score_is player_1_score, player_2_score
     expect(tennis_game.player_1_score).to eq player_1_score
     expect(tennis_game.player_2_score).to eq player_2_score
   end
-  
+
 end
